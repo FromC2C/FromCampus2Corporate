1cf2bac79e510015f0cf6f8db444417eba5b668f
Amelie Tam <atam2@students.cs.ubc.ca>
2025-06-25 09:49:49 -0400

    testimonials section added

---
+++ script.js
@@ -0,0 +1,36 @@
+const texts = document.querySelectorAll('.carousel-text');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let index = 0;
+let interval = setInterval(showNextText, 3000);
+
+function showText(i) {
+  texts[index].classList.remove('active');
+  index = (i + texts.length) % texts.length;
+  texts[index].classList.add('active');
+}
+
+function showNextText() {
+  showText(index + 1);
+}
+
+function showPrevText() {
+  showText(index - 1);
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(showNextText, 3000);
+}
+
+// Event listeners for buttons
+nextBtn.addEventListener('click', () => {
+  showNextText();
+  resetInterval();
+});
+
+prevBtn.addEventListener('click', () => {
+  showPrevText();
+  resetInterval();
+});
